af40662a04dee2d1e0a2a84ca11eba22b94acc18
balanasia <anabalasia@gmail.com>
2022-06-30 15:39:00 -0500

    Add chained route to look up a specific article. Add GET and PUT routes to search a specific article

--- app.js
+++ app.js
@@ -28,29 +28,67 @@ app.get("/", function(req, res) {
   res.send("Hello mom c:");
 });
 
-app.get("/articles", function(req, res) {
-  Article.find({}, function(err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      console.log(err);
-    }
-  });
-});
+////////Requests Targeting a All Article//////////////////
 
-app.post("/articles", function(req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article!");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted articles");
+      } else {
+        res.send("err");
+      }
+    });
   });
-  newArticle.save(function(err){
-    if(!err){
-      res.send("Successfully added a new article!");
-    } else {
-      res.send(err);
-    }
+
+////////Requests Targeting a Specific Article//////////////////
+
+app.route("/articles/:articleTitle")
+  .get(function(req, res) {
+    Article.findOne({
+      title: req.params.articleTitle
+    }, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        console.log("No article found");
+      }
+    });
+  })
+  .put(function(req, res) {
+    Article.replaceOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      function(err){
+        if(!err){
+          res.send("Successfully updated the article!");
+        }
+      }
+    );
   });
-});
 
 
 app.listen(3000, function() {
