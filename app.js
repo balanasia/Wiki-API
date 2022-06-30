e308f1297242d9421674b9d3e081592f0c46288a
balanasia <anabalasia@gmail.com>
2022-06-30 09:23:29 -0500

    Add /articles route to display articles stored in the database

---
+++ app.js
@@ -0,0 +1,44 @@
+//intialize packages
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+//initialize DB name
+mongoose.connect('mongodb://localhost:27017/wikiDB');
+
+const articleSchema = {
+  title: String,
+  content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", function(req, res){
+  res.send("Hello mom c:");
+});
+
+app.get("/articles", function(req, res){
+  Article.find({}, function(err, foundArticles){
+    if(!err) {
+      res.send(foundArticles);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
